c9e54abca7cd81fb7d2e6b05ed82130b9949dbb7
OverThunder <56888147+OverThunder@users.noreply.github.com>
2020-01-30 14:57:55 -0600

    Removing Everythin

--- venv/Scripts/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!c:\users\bret\documents\visual_studio_code\python\software_engineering_v\d2d-test-bookstore\venv\scripts\python.exe
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
