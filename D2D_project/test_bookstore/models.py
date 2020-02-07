7243ccd401687b54c824b628882e26fb5fe89a84
JamesEHannah <35670901+JamesEHannah@users.noreply.github.com>
2020-02-07 15:22:58 -0600

    Updated Book model with non-required fields

--- D2D_project/test_bookstore/models.py
+++ D2D_project/test_bookstore/models.py
@@ -5,13 +5,13 @@ class Book(models.Model):
     ISBN = models.CharField(max_length=13, primary_key=True)
     title = models.CharField(max_length=200)
     primary_author = models.CharField(max_length=100)
-    secondary_authors = models.CharField(max_length=200)
+    secondary_authors = models.CharField(max_length=200, default=None, blank=True, null=True)
     description = models.TextField()
     publisher = models.CharField(max_length=200)
-    release_date = models.CharField(max_length=200)
+    release_date = models.CharField(max_length=15)
     price = models.CharField(max_length=50)
-    series = models.CharField(max_length=200)
-    volume_number = models.CharField(max_length=10)
+    series = models.CharField(max_length=200, default=None, blank=True, null=True)
+    volume_number = models.CharField(max_length=10, default=None, blank=True, null=True)
 
     def __str__(self):
         return f' {self.ISBN}, {self.title}, {self.author}, {self.secondary_authors}'
