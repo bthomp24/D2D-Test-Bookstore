f935c1673e9f8947a42974b8ff594f3262d338f7
Brandon Thompson <thompsonbrandon13@yahoo.com>
2020-02-10 14:04:21 -0600

    Took ut comments

--- D2D_project/test_bookstore/models.py
+++ D2D_project/test_bookstore/models.py
@@ -18,5 +18,4 @@ class Book(models.Model):
 
 class File(models.Model):
     f = models.FileField(blank=False, null=False)
-    #remark = models.CharField(max_length=20)
     timestamp = models.DateTimeField(auto_now_add=True)
--- D2D_project/test_bookstore/urls.py
+++ D2D_project/test_bookstore/urls.py
@@ -1,17 +1,11 @@
 from django.urls import include, path
-#from rest_framework import routers
-#from . import views
 from .views import FileView
 from django.conf.urls import url
 
-# router = routers.DefaultRouter()
-# router.register(r'views', views.BookViewSet)
-#router.register(r'upload', FileView.as_view() ) #name='file-upload'
 
 # Wire up our API using automatic URL routing.
 # Additionally, we include login URLs for the browsable API.
 urlpatterns = [
-    #path('views/', include(router.urls)),
-    #path('api-auth/', include('rest_framework.urls', namespace='rest_framework'))
+    path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
     path('upload/', FileView.as_view(), name='file-upload'),
 ]
