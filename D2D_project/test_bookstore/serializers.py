55b205fbf3db18da863690fa1fed0e3c3369f13b
Brandon Thompson <thompsonbrandon13@yahoo.com>
2020-02-11 09:47:17 -0600

    updated bookserializer

--- D2D_project/test_bookstore/serializers.py
+++ D2D_project/test_bookstore/serializers.py
@@ -5,7 +5,7 @@ from .models import File
 class BookSerializer (serializers.HyperlinkedModelSerializer):
     class Meta:
         model = Book
-        fields = ('ISBN', 'title', 'author', 'secondary_authors', 'description', 
+        fields = ('ISBN', 'title', 'primary_author', 'secondary_authors', 'description', 
                 'publisher', 'release_date', 'price', 'series', 
                 'volume_number', 'is_available',)
 
