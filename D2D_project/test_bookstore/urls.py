b90ceb54137cef7f8bc5b0d59c206750b3c9766e
Daniel-Verse <59664339+Daniel-Verse@users.noreply.github.com>
2020-02-11 08:16:23 -0600

    stuff

--- D2D_project/test_bookstore/urls.py
+++ D2D_project/test_bookstore/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from . import views
 
+app_name = 'test_bookstore'
 urlpatterns = [
     # Put the page URL's individually for the thing.
     path('', views.search, name='search'),
