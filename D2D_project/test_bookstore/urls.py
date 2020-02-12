6723d5174824973d7c80f4308fdaab8fa68c59f4
Daniel-Verse <59664339+Daniel-Verse@users.noreply.github.com>
2020-02-12 12:48:36 -0600

    updation of the urls and html pages

--- D2D_project/test_bookstore/urls.py
+++ D2D_project/test_bookstore/urls.py
@@ -5,6 +5,6 @@ app_name = 'test_bookstore'
 urlpatterns = [
     # Put the page URL's individually for the thing.
     path('', views.search, name='search'),
-    path('details/', views.detail, name='detail'),
+    path('<int:pk>/details', views.bookDetail.as_view(), name='book_detail'),
 
 ]
--- D2D_project/test_bookstore/views.py
+++ D2D_project/test_bookstore/views.py
@@ -1,15 +1,32 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
 from django.template import loader
+from django.views import generic
 
-# Create your views here.
+from .models import Book
+
 
 def search(request):
     template = loader.get_template('test_bookstore/search.html')
 
-    return render(request, 'test_bookstore/search.html')
+    results = Book.objects.all()
 
-def detail(request):
-    template = loader.get_template('test_bookstore/detail.html')
+    context = {
+        'results': results,
+    }
 
-    return render(request, 'test_bookstore/detail.html')
+    return render(request, 'test_bookstore/search.html', context=context)
+
+class bookDetail(generic.DetailView):
+    model = Book
+
+    def detail(self, request, primary_key):
+        template = loader.get_template('test_bookstore/book_detail.html')
+
+        book = get_object_or_404(Book, pk=primary_key)
+
+        context = {
+            'book': book
+            }
+
+        return render(request, 'test_bookstore/book_detail.html', context=context)
