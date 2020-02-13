331e14dd98abcc2bf3df1cd1d3d586974bd832f8
Daniel-Verse <59664339+Daniel-Verse@users.noreply.github.com>
2020-02-12 20:49:06 -0600

    The next to final edits for the pages

--- D2D_project/test_bookstore/urls.py
+++ D2D_project/test_bookstore/urls.py
@@ -6,5 +6,4 @@ urlpatterns = [
     # Put the page URL's individually for the thing.
     path('', views.search, name='search'),
     path('<int:pk>/details', views.bookDetail.as_view(), name='book_detail'),
-
 ]
