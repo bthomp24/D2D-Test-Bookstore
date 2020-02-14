9dd3513b443d76176cbf902643ebeb8b6c0b4eb2
Daniel-Verse <59664339+Daniel-Verse@users.noreply.github.com>
2020-02-14 03:51:44 -0600

    The finality of the updates for now

--- D2D_project/test_bookstore/views.py
+++ D2D_project/test_bookstore/views.py
@@ -1,61 +1,119 @@
 from django.shortcuts import render, get_object_or_404
-from django.http import HttpResponse
-from django.template import loader
+
+from django.template import loader, RequestContext
 from django.views import generic
-from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+
+from django_tables2.config import RequestConfig
 
 from .models import Book
+from .tables import ResultsTable
 from .forms import searchBar
 
 from .search import Search_Query
 
-
+"""
+args:
+    Http request: Requests from either POST or GET. Will
+    be used for list display, searcher, and to paginate.
+returns:
+    An Http Render: A render of the Search Bar Form and
+    Results Table to be displayed in "search.html"
+synopsis:
+    The view serves as the search page as well as listing
+    the search results from the query made. It will also
+    provide options to visit each individual entry with a
+    "details" link. It will contain all the books from the
+    database and list them accordingly with no cut off point,
+    though one could be implemented in the future.
+"""
 def search(request):
 
-    """
-    if not request.method == 'POST':
-        if 'search-persons-post' in request.session:
-            request.POST = request.session['search-persons-post']
+    # empty list that will hold our search display data for
+    # the ResultsTable    
+    results_display = []
+
+    # POST forcer for our "page" GET request. So we can
+    # access our old POST data when we move to the next page.
+    if not request.method == 'POST' and 'page' in request.GET:
+        if 'searcher' in request.session:
+            request.POST = request.session['searcher']
             request.method = 'POST'
-            """
+
     
-    if request.method == 'POST' or 'page' in request.GET:
+    if request.method == 'POST':
 
+        # Acquiring the user's input on the search bar
         form = searchBar(request.POST)
+        request.session['searcher'] = request.POST
 
+        # checking data and using it for our Search_Query function.
         if form.is_valid():
 
             searcher = form.cleaned_data['searcher']
 
+            # Receiving results in a list of [Book Object, float]
             results = Search_Query(searcher)
 
-            paginator = Paginator(results, 5)
-            page_number = request.GET.get('page')
-            page_obj = paginator.get_page(page_number)
 
-            context = {
-                'page_obj': page_obj,
-                'results': results,
-                'form': form,
-                }
+            # Loop for constructing the list of dictionaries to build our ResultsTable
+            for books in results:
+                dicts = {}
+                dicts["title"] = getattr(books[0], "title")
+                dicts["author"] = getattr(books[0], "primary_author")
+                dicts["ISBN"] = getattr(books[0], "ISBN")
+                dicts["rating"] = books[1]
+                results_display.append(dicts)            
     else:
-        
+        # Purposes of hiding the list until a query is made.
         form = searchBar()
-        context = {
-        'form': form,
-        }
 
-            
-    
+    # Building our Results Table from the list of dictionary data.
+    # and creating the paginator for the results.
+    results_table = ResultsTable(results_display)
+    RequestConfig(request, paginate={"per_page": 5}).configure(results_table)
+
+    context = {
+        'table': results_table,
+        'results': results,
+        'form': form,
+    }
 
     return render(request, 'test_bookstore/search.html', context=context)
 
 
+"""
+args:
+    generic Detail View: for creating the Book Detail class view
+returns:
+    not applicable
+synopsis:
+    This is the class that will allow direct mapping to our
+    database for displaying book details in the book detail
+    view. This will simplify the connections and it's primary
+    function will be responsible for location and passing in the
+    render.
+"""
 class bookDetail(generic.DetailView):
     model = Book
 
+    """
+    args:
+        Http request: to acquire the request from the browser to
+        open the page.
+        primary key: This will be the key we use to locate the
+        specific book in the database that the user was wanting to
+        see the details about.
+    returns:
+        An Http Render: This render will be for our book data's context
+        as well as directing to the 'book_detail.html' file.
+    synopsis:
+        This is our other page whose purpose is to display the
+        various bits of information about books from the database.
+        To reach this page, a user must click on the "details" link
+        from their queries and will be brought to the details page with
+        all the book data displayed accordingly.
+    """
     def detail(self, request, primary_key):
-        template = loader.get_template('test_bookstore/book_detail.html')
 
         book = get_object_or_404(Book, pk=primary_key)
 
