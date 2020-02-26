345303cd4b734f3a501d34c97f9cc226f6227e7d
Daniel-Verse <59664339+Daniel-Verse@users.noreply.github.com>
2020-02-26 13:56:36 -0600

    Minor Base start

---
+++ Sprint_02/parser_google.py
@@ -0,0 +1,17 @@
+import io
+from lxml import etree
+import requests
+
+
+def __get_title():
+    pass
+def __get_author():
+    pass
+def __get_isbn():
+    pass
+def __get_description():
+    pass
+def __get_image():
+    pass
+def __get_series():
+    pass
