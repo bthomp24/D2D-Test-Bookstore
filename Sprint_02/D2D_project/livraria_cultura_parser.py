f0677dc7fff8838dca8b5826d423e9c8028d1783
Brandon Thompson <thompsonbrandon13@yahoo.com>
2020-02-26 14:30:06 -0600

    livraria cultura parser

---
+++ Sprint_02/D2D_project/livraria_cultura_parser.py
@@ -0,0 +1,56 @@
+import io
+from lxml import etree
+import requests
+import Parent_Scrape
+
+URL = "https://www3.livrariacultura.com.br/harry-potter-e-o-prisioneiro-de-azkaban-2010606463/p"
+
+def fetch(url):
+    response = requests.get(url)
+    return response.content
+
+def parse_for_title(content):
+    return Parent_Scrape.parse(content, ".//h1[@class='title_product']/div")[0].text
+
+def parse_for_subtitle(content):
+    subtitle_element = Parent_Scrape.parse(content, "//*[@class='value-field Subtitulo']")
+    if len(subtitle_element)>0:
+        subtitle = subtitle_element[0].text
+        return subtitle
+
+def parse_for_book_image_url(content):
+    return Parent_Scrape.parse(content, "//*[@id='image-main']/@src")[0]
+
+def parse_for_isbn(content):
+    return Parent_Scrape.parse(content, "//*[@class='value-field ISBN']")[0].text
+
+def parse_for_description(content):
+    return Parent_Scrape.parse(content, "//*[@class='value-field Sinopse']")[0].text
+
+def parse_for_authors(content):
+    authors_element = Parent_Scrape.parse(content, "//*[@class='value-field Colaborador']")[0]
+    authors = authors_element.text
+    authors = authors.replace('Autor:', '')
+    authors = authors.replace('Tradutor:', '')
+    return authors
+
+
+content = fetch(URL)
+f = open("html.txt", "wb")
+f.write(content)
+
+book_title = parse_for_title(content)
+subtitle = parse_for_subtitle(content)
+book_image_url = parse_for_book_image_url(content)
+isbn_13 = parse_for_isbn(content)
+description = parse_for_description(content)
+authors = parse_for_authors(content)
+    
+
+print('Title: ', book_title)
+print('Subtile: ', subtitle)
+print('Book Image URL: ', book_image_url)
+print('ISBN13: ', isbn_13)
+print('Description: ', description)
+print('Authors: ', authors)
+    
