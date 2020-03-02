cae05c6afddcc0be4af48b4e97740280772a3465
Brandon Thompson <thompsonbrandon13@yahoo.com>
2020-03-02 14:02:54 -0600

    added parse functions

--- Sprint_02/D2D_project/livraria_cultura_parser.py
+++ Sprint_02/D2D_project/livraria_cultura_parser.py
@@ -3,7 +3,7 @@ from lxml import etree
 import requests
 import Parent_Scrape
 
-URL = "https://www3.livrariacultura.com.br/harry-potter-e-o-prisioneiro-de-azkaban-2010606463/p"
+URL = "https://www3.livrariacultura.com.br/harry-potter-and-philosophy-107221717/p"
 
 def fetch(url):
     response = requests.get(url)
@@ -25,19 +25,28 @@ def parse_for_isbn(content):
     return Parent_Scrape.parse(content, "//*[@class='value-field ISBN']")[0].text
 
 def parse_for_description(content):
-    return Parent_Scrape.parse(content, "//*[@class='value-field Sinopse']")[0].text
+    return Parent_Scrape.parse(content, "//meta[@itemprop='description']/@content")[0]
 
 def parse_for_authors(content):
     authors_element = Parent_Scrape.parse(content, "//*[@class='value-field Colaborador']")[0]
     authors = authors_element.text
     authors = authors.replace('Autor:', '')
     authors = authors.replace('Tradutor:', '')
+    authors = authors.replace('Coordenador/Editor:', '')
     return authors
 
+def parse_for_book_id(content):
+    return Parent_Scrape.parse(content, "//meta[@itemprop='gtin13']/@content")[0]
+
+def parse_for_url(content):
+    return Parent_Scrape.parse(content, "//meta[@itemprop='url']/@content")[0]
+
+# def parse_for_ready_for_sale(content):
+#     return Parent_Scrape.parse(content, "//*[@class='value-field Sinopse']")[0].text
 
 content = fetch(URL)
-f = open("html.txt", "wb")
-f.write(content)
+# f = open("html.txt", "wb")
+# f.write(content)
 
 book_title = parse_for_title(content)
 subtitle = parse_for_subtitle(content)
@@ -45,7 +54,8 @@ book_image_url = parse_for_book_image_url(content)
 isbn_13 = parse_for_isbn(content)
 description = parse_for_description(content)
 authors = parse_for_authors(content)
-    
+book_id = parse_for_book_id(content)
+url = parse_for_url(content)
 
 print('Title: ', book_title)
 print('Subtile: ', subtitle)
@@ -53,4 +63,5 @@ print('Book Image URL: ', book_image_url)
 print('ISBN13: ', isbn_13)
 print('Description: ', description)
 print('Authors: ', authors)
-    
+print('Book ID: ', book_id)
+print('URL: ', url)
