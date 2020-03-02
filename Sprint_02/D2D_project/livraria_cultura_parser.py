619b0ba6126294d0f4794e0cf3e87036335f9012
Brandon Thompson <thompsonbrandon13@yahoo.com>
2020-03-02 14:07:58 -0600

    change to parent_scrape_threading

--- Sprint_02/D2D_project/livraria_cultura_parser.py
+++ Sprint_02/D2D_project/livraria_cultura_parser.py
@@ -1,7 +1,7 @@
 import io
 from lxml import etree
 import requests
-import Parent_Scrape
+import Parent_Scrape_Threading
 
 URL = "https://www3.livrariacultura.com.br/harry-potter-and-philosophy-107221717/p"
 
@@ -10,25 +10,25 @@ def fetch(url):
     return response.content
 
 def parse_for_title(content):
-    return Parent_Scrape.parse(content, ".//h1[@class='title_product']/div")[0].text
+    return Parent_Scrape_Threading.parse(content, ".//h1[@class='title_product']/div")[0].text
 
 def parse_for_subtitle(content):
-    subtitle_element = Parent_Scrape.parse(content, "//*[@class='value-field Subtitulo']")
+    subtitle_element = Parent_Scrape_Threading.parse(content, "//*[@class='value-field Subtitulo']")
     if len(subtitle_element)>0:
         subtitle = subtitle_element[0].text
         return subtitle
 
 def parse_for_book_image_url(content):
-    return Parent_Scrape.parse(content, "//*[@id='image-main']/@src")[0]
+    return Parent_Scrape_Threading.parse(content, "//*[@id='image-main']/@src")[0]
 
 def parse_for_isbn(content):
-    return Parent_Scrape.parse(content, "//*[@class='value-field ISBN']")[0].text
+    return Parent_Scrape_Threading.parse(content, "//*[@class='value-field ISBN']")[0].text
 
 def parse_for_description(content):
-    return Parent_Scrape.parse(content, "//meta[@itemprop='description']/@content")[0]
+    return Parent_Scrape_Threading.parse(content, "//meta[@itemprop='description']/@content")[0]
 
 def parse_for_authors(content):
-    authors_element = Parent_Scrape.parse(content, "//*[@class='value-field Colaborador']")[0]
+    authors_element = Parent_Scrape_Threading.parse(content, "//*[@class='value-field Colaborador']")[0]
     authors = authors_element.text
     authors = authors.replace('Autor:', '')
     authors = authors.replace('Tradutor:', '')
@@ -36,10 +36,10 @@ def parse_for_authors(content):
     return authors
 
 def parse_for_book_id(content):
-    return Parent_Scrape.parse(content, "//meta[@itemprop='gtin13']/@content")[0]
+    return Parent_Scrape_Threading.parse(content, "//meta[@itemprop='gtin13']/@content")[0]
 
 def parse_for_url(content):
-    return Parent_Scrape.parse(content, "//meta[@itemprop='url']/@content")[0]
+    return Parent_Scrape_Threading.parse(content, "//meta[@itemprop='url']/@content")[0]
 
 # def parse_for_ready_for_sale(content):
 #     return Parent_Scrape.parse(content, "//*[@class='value-field Sinopse']")[0].text
