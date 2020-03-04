1f04fd41beaaa1f5a5ed0d7f684d8ded8bbc90c1
Brandon Thompson <thompsonbrandon13@yahoo.com>
2020-03-04 14:26:04 -0600

    added convert to url function

--- Sprint_02/D2D_project/livraria_cultura_parser.py
+++ Sprint_02/D2D_project/livraria_cultura_parser.py
@@ -31,13 +31,16 @@ class book_site_livraria_cultura():
     def __get_authors__(self, content):
         authors_element = Parent_Scrape_Threading.parse(content, "//*[@class='value-field Colaborador']")[0]
         authors = authors_element.text
-        authors = authors.replace('Autor:', '')
-        authors = authors.replace('Tradutor:', '')
-        authors = authors.replace('Coordenador/Editor:', '')
+        # authors = authors.replace('Autor:', '')
+        # authors = authors.replace('Tradutor:', '')
+        # authors = authors.replace('Coordenador/Editor:', '')
         return authors
 
     def __get_book_id__(self, content):
-        return Parent_Scrape_Threading.parse(content, "//meta[@itemprop='gtin13']/@content")[0]
+        book_id = self.__get_url__(content)
+        book_id = book_id.replace('https://www3.livrariacultura.com.br/', '')
+        book_id = book_id.replace('/p', '')
+        return book_id
 
     def __get_url__(self, content):
         return Parent_Scrape_Threading.parse(content, "//meta[@itemprop='url']/@content")[0]
@@ -66,34 +69,67 @@ class book_site_livraria_cultura():
         ready_for_sale = None
         extra = None
 
-        
-        # f = open("html.txt", "wb")
-        # f.write(content)
+        #format
         format = 'DIGITAL'
+
+        #book_title
         book_title = self.__get_title__(content)
+
+        #subtitle
         subtitle = self.__get_subtitle__(content)
+
+        #book_image_url
         book_image_url = self.__get_book_image_url__(content)
+
+        #book_image
         book_image = Parent_Scrape_Threading.get_book_image_from_image_url(book_image_url)
+
+        #isbn_13
         isbn_13 = self.__get_isbn__(content)
+
+        #description
         description = self.__get_description__(content)
+
+        #authors
         authors = self.__get_authors__(content)
+
+        #book_id
         book_id = self.__get_book_id__(content)
+
+        #site_slug
         site_slug = 'LC'
+
+        #url
         url = self.__get_url__(content)
+
+        #ready_for_sale
         ready_for_sale = True
+
+
         #price = None
 
         if subtitle == None:
-            parse_list = [format, book_title, book_image, book_image_url, isbn_13, description, subtitle, authors, book_id, site_slug, url, content, ready_for_sale]
-        else:
             parse_list = [format, book_title, book_image, book_image_url, isbn_13, description, authors, book_id, site_slug, url, content, ready_for_sale]
+        else:
+            parse_list = [format, book_title, book_image, book_image_url, isbn_13, description, subtitle, authors, book_id, site_slug, url, content, ready_for_sale]
 
+        #parse_status
         parse_status = Parent_Scrape_Threading.parse_status(parse_list)
 
         SiteBookData = [format, book_title, book_image, book_image_url, isbn_13, description, series, volume_number, subtitle, authors, book_id, site_slug, parse_status, url, content, ready_for_sale, extra]
 
         return SiteBookData
 
-URL = "https://www3.livrariacultura.com.br/harry-potter-and-philosophy-107221717/p"
+    def find_book_matches_at_site(self, book_data):
+        return None
+
+    def convert_book_id_to_url(self, book_id):
+        primary_url = "https://www3.livrariacultura.com.br/"
+        return primary_url + book_id + "/p"
+
+
+URL = "https://www3.livrariacultura.com.br/harry-potter-e-o-prisioneiro-de-azkaban-2010606463/p"
 lc = book_site_livraria_cultura()
 print(lc.get_book_data_from_site(URL))
+
+print(lc.convert_book_id_to_url('harry-potter-e-o-prisioneiro-de-azkaban-2010606463'))
