b681a8c0cde9c2d7e62c4c210bab535174a2d8e5
Daniel-Verse <59664339+Daniel-Verse@users.noreply.github.com>
2020-03-06 17:04:28 -0600

    Google Books search page and form submission edits

--- Sprint_02/Scrapers.py
+++ Sprint_02/Scrapers.py
@@ -1,8 +1,11 @@
 import Parsers.Main_Scribd as scribd_book_site
+import Parsers.Main_Google_Books as google_book_site
 
 def get_book_site(slug):
     if slug.lower() == "sd":
         return scribd_book_site.book_site_scribd()
+    else if slug.lower() == "GB":
+        return google_book_site.book_site_google()
     else:
         return None
 
