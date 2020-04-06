8a3abf625695cf210eb55d66522b8aa4a8881189
JamesEHannah <35670901+JamesEHannah@users.noreply.github.com>
2020-04-05 22:19:36 -0500

    Added loading page

--- Sprint_03/B2B_Project/b2b_site/clients/urls.py
+++ Sprint_03/B2B_Project/b2b_site/clients/urls.py
@@ -4,9 +4,11 @@ from django.contrib import admin
 from django.urls import include
 from django.views.generic import RedirectView
 from .views import MainView
+from django.views.generic import TemplateView
 
 urlpatterns = [
     path('', views.login, name = 'login_form'),
     path('search/', MainView.as_view(), name='search'),
     path('manage_account/', views.manage_account, name= 'manage_account'),
+    path('loading/',TemplateView.as_view(template_name='loading.html'))
 ]
