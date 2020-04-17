b6f63b920a49387b3269b68115a3f2f87ff51e5e
Brandon Thompson <thompsonbrandon13@yahoo.com>
2020-04-17 13:47:25 -0500

    reset db and fixed urls format issue

--- Sprint_03/B2B_Project/b2b_site/clients/urls.py
+++ Sprint_03/B2B_Project/b2b_site/clients/urls.py
@@ -10,7 +10,6 @@ urlpatterns = [
     path('', views.login, name = 'login_form'),
     path('search/', MainView.as_view(), name='search'),
     path('checkmate/', CheckmateView.as_view(), name='search-checkmate'),
-]
     path('manage_account/', views.manage_account, name= 'manage_account'),
     path('query_report/', views.myFirstChart, name= 'query_report'),
     path('query_chart/', views.historicalChart, name= 'query_chart'),
